c5f3c65ce1210fc31b77f2dfe32f5ba21922f364
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-09-07 11:30:05 -0500

    "modify project configuration in pom.xml file"

--- src/main/java/com/example/finanzasgrupo4/DemoApplication.java
+++ src/main/java/com/example/finanzasgrupo4/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.finanzasgrupo4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
