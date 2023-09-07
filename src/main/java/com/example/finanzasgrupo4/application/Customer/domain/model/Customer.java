57ad828c2b98b4e1fbb70b63e8fd9de080fcab6f
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-09-07 12:03:22 -0500

    feat(Customer): add customer model entity

---
+++ src/main/java/com/example/finanzasgrupo4/application/Customer/domain/model/Customer.java
@@ -0,0 +1,30 @@
+package com.example.finanzasgrupo4.application.Customer.domain.model;
+
+import com.example.finanzasgrupo4.shared.domain.AuditModel;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="customer")
+public class Customer extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @NotBlank
+    private String email;
+    @NotNull
+    private Long identityDoc;
+
+}
