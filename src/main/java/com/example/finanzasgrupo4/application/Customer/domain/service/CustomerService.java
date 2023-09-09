437b75d84611db4a7080dbc6950dc85d54867002
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-09-09 14:31:43 -0500

    feat(Customer):add service

--- src/main/java/com/example/finanzasgrupo4/application/Customer/domain/service/CustomerService.java
+++ src/main/java/com/example/finanzasgrupo4/application/Customer/domain/service/CustomerService.java
@@ -1,11 +1,17 @@
 package com.example.finanzasgrupo4.application.Customer.domain.service;
 
 import com.example.finanzasgrupo4.application.Customer.domain.model.Customer;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface CustomerService {
     List<Customer> getAll();
-    Customer getByUsernameAndPassword(String username, String password);
+    Customer getByEmailAndPassword(String username, String password);
     Customer getByUsername(String username);
+    Customer getByEmail(String email);
+    Customer getById(Long customerId);
+    Customer create(Customer customer);
+    Customer update(Long customerId, Customer request);
+    ResponseEntity<?> delete(Long customerId);
 }
--- src/main/java/com/example/finanzasgrupo4/application/Customer/service/CustomerServiceImp.java
+++ src/main/java/com/example/finanzasgrupo4/application/Customer/service/CustomerServiceImp.java
@@ -1,23 +1,106 @@
 package com.example.finanzasgrupo4.application.Customer.service;
 
 import com.example.finanzasgrupo4.application.Customer.domain.model.Customer;
+import com.example.finanzasgrupo4.application.Customer.domain.persistence.CustomerRepository;
 import com.example.finanzasgrupo4.application.Customer.domain.service.CustomerService;
+import com.example.finanzasgrupo4.shared.exception.ResourceNotFoundException;
+import com.example.finanzasgrupo4.shared.exception.ResourceValidationException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
+@Service
 public class CustomerServiceImp implements CustomerService {
+    private static final String ENTITY="Reward";
+    private final CustomerRepository customerRepository;
+    private final Validator validator;
+
+    public CustomerServiceImp(CustomerRepository customerRepository, Validator validator){
+        this.customerRepository=customerRepository;
+        this.validator =validator;
+    }
     @Override
     public List<Customer> getAll() {
-        return null;
+        return customerRepository.findAll();
     }
 
     @Override
-    public Customer getByUsernameAndPassword(String username, String password) {
-        return null;
+    public Customer getByEmailAndPassword(String email, String password) {
+        return customerRepository.findByEmailAndPassword(email, password);
     }
 
     @Override
     public Customer getByUsername(String username) {
-        return null;
+        return customerRepository.findByUsername(username);
+    }
+
+    @Override
+    public Customer getByEmail(String email) {
+        return customerRepository.findByEmail(email);
+    }
+
+    @Override
+    public Customer getById(Long customerId) {
+        return customerRepository.findById(customerId).orElseThrow(()->
+                new ResourceNotFoundException(ENTITY,customerId));
+    }
+
+    @Override
+    public Customer create(Customer customer) {
+        Set<ConstraintViolation<Customer>> violations = validator.validate(customer);
+
+        if(!violations.isEmpty())
+            throw new ResourceValidationException(ENTITY, violations);
+
+        Customer customerWithUsername = customerRepository.findByUsername(customer.getEmail());
+
+        if (customerWithUsername != null)
+            throw new ResourceValidationException(ENTITY,
+                    "A customer with the same username already exists.");
+
+        Customer customerWithEmail = customerRepository.findByEmail(customer.getEmail());
+
+        if (customerWithEmail != null)
+            throw new ResourceValidationException(ENTITY,
+                    "A customer with the same email already exists.");
+
+        Customer customerWithIdentityDoc = customerRepository.findByIdentityDoc(customer.getIdentityDoc());
+
+        if (customerWithIdentityDoc != null)
+            throw new ResourceValidationException(ENTITY,
+                    "A customer with the same identity document number already exists.");
+
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public Customer update(Long customerId, Customer request) {
+        Set<ConstraintViolation<Customer>> violations = validator.validate(request);
+
+        if (!violations.isEmpty())
+            throw new ResourceValidationException(ENTITY, violations);
+
+        return customerRepository.findById(customerId).map(customer ->
+                        customerRepository.save(customer.withFirstName(request.getFirstName())
+                                .withLastName(request.getLastName())
+                                .withEmail(request.getEmail())
+                                .withIdentityDoc(request.getIdentityDoc())
+                                .withUsername(request.getUsername())
+                                .withPassword(request.getPassword())
+                                ))
+                .orElseThrow(() -> new ResourceNotFoundException(ENTITY, customerId));
+    }
+
+    @Override
+    public ResponseEntity<?> delete(Long customerId) {
+        return customerRepository.findById(customerId).map(
+                customer -> {
+                    customerRepository.delete(customer);
+                    return ResponseEntity.ok().build();
+                }).orElseThrow(()->new ResourceNotFoundException(ENTITY,customerId));
     }
 }
